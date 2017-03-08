dcf1bf3032b19157db3243bc7e4d4c8f255bc7d1
Ioannis G <jtgftw@gmail.com>
2017-03-08 17:54:16 +0200

    start project

---
+++ Wox.Plugin.Github/GithubPlugin.fs
@@ -0,0 +1,16 @@
+﻿namespace Wox.Plugin.Github
+
+open Octokit
+open Wox.Plugin
+open System.Collections.Generic
+
+type GithubPlugin() = 
+    
+    let mutable PluginContext = new PluginInitContext()
+
+    interface IPlugin with
+        member this.Init (context:PluginInitContext) = 
+            PluginContext <- context
+
+        member this.Query q =
+            List<Result> [ new Result() ]
---
+++ Wox.Plugin.Github/Script.fsx
@@ -0,0 +1,8 @@
+﻿#I "../packages/Octokit/lib/net45"
+#I "../packages/Wox.Plugin/lib/net452"
+
+#r "Octokit.dll"
+#r "Wox.Plugin.dll"
+
+#load "GithubPlugin.fs"
+open Wox.Plugin.Github
